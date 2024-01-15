06f68ddea481f115527ab41dcc1adfa272fee21b
Trefor Coster <trefor@savvi.studio>
2024-01-15 06:53:33 +0000

    updates

--- wp-content/themes/lonic/resources/views/components/cards/card-post-archive.blade.php
+++ wp-content/themes/lonic/resources/views/components/cards/card-post-archive.blade.php
@@ -22,6 +22,22 @@
           <h3><?php the_title(); ?></h3>
 
           <?php the_excerpt();?>
+
+          <a class="more" href="<?php the_permalink();?>">Read more
+            <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24.978 17.843">
+              <defs>
+                <style>
+                  .arrow-colour {
+                    fill: none;
+                    stroke-width: 2px;
+                  }
+                </style>
+              </defs>
+              <g id="Group_6" data-name="Group 6" transform="translate(-514.5 -1549.798)">
+                <line id="Line_3" data-name="Line 3" class="arrow-colour" x2="23.029" transform="translate(514.5 1558.725)" />
+                <path id="Path_2" data-name="Path 2" class="arrow-colour" d="M533.163,1550.505l8.215,8.215-8.215,8.214" transform="translate(-3.314)" />
+              </g>
+            </svg></a>
          
         </div>  
       </div>  
@@ -49,7 +65,22 @@
 
           <?php the_excerpt();?>
          
-           
+          <a class="more" href="<?php the_permalink();?>">Read more
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24.978 17.843">
+              <defs>
+                <style>
+                  .arrow-colour {
+                    fill: none;
+                    stroke-width: 2px;
+                  }
+                </style>
+              </defs>
+              <g id="Group_6" data-name="Group 6" transform="translate(-514.5 -1549.798)">
+                <line id="Line_3" data-name="Line 3" class="arrow-colour" x2="23.029" transform="translate(514.5 1558.725)" />
+                <path id="Path_2" data-name="Path 2" class="arrow-colour" d="M533.163,1550.505l8.215,8.215-8.215,8.214" transform="translate(-3.314)" />
+              </g>
+            </svg>
+          </a>
       
     </div>
   
